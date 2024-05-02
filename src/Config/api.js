0a1988cebf5c74801eca02a99296fe163c6d9638
Manikandan N <99782666+Starlord48@users.noreply.github.com>
2024-05-02 08:43:44 +0530

    Update global API in api.js

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL="http://localhost:8080/Twitter"
+export const API_BASE_URL="https://backend-twitter-api.onrender.com/Twitter"
 
 export const api = axios.create({
     baseURL:API_BASE_URL,
